106256d567798f7c8d43c2d9fd0291a122b6f078
dk_laptop <dominik_schaefer97@yahoo.com>
2018-10-08 13:27:45 +0200

    Erster Commit

---
+++ UnitTests.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTests {
+
+    @Test
+    public void ErsterTest(){
+        assertEquals(2,1);
+    }
+}
