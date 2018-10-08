e9698d1a6d7bfb6ea3a8835b617121ececada41f
dk_laptop <dominik_schaefer97@yahoo.com>
2018-10-08 13:42:09 +0200

    Basisklassen erstellt

--- UnitTests.java
+++ UnitTests.java
@@ -7,7 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UnitTests {
 
     @Test
-    public void ErsterTest(){
-        assertEquals(2,1);
+    public void ZooObjektKannErstelltWerden(){
+        Zoo meinZoo = new Zoo();
+    }
+
+    @Test
+    public void KassenObjektKannErstelltWerden(){
+        Kasse meineKasse= new Kasse();
+    }
+
+    @Test
+    public void TicketObjektKannErstelltWerden(){
+        Ticket meinTicket = new Ticket();
+    }
+
+    @Test
+    public void BesucherObjektKannErstelltWerden(){
+        Besucher meinBesucher = new Besucher();
     }
 }
